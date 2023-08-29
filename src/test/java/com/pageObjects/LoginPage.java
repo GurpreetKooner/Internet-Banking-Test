89baf27814af54653d033902fc3ead397d9f1d1e
Gurpreet Singh <gurpreet1999kooner@gmail.com>
2023-08-30 01:20:29 +0530

    Login PageObject created

---
+++ src/test/java/com/pageObjects/LoginPage.java
@@ -0,0 +1,44 @@
+package com.pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+	WebDriver driver;
+
+	LoginPage(WebDriver driver) {
+
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+
+	}
+
+	@FindBy(name = "uid")
+	@CacheLookup
+	WebElement txtUserId;
+
+	@FindBy(name = "password")
+	@CacheLookup
+	WebElement txtPassword;
+
+	@FindBy(name = "btnLogin")
+	@CacheLookup
+	WebElement btnLogin;
+
+	public void setUserId(String uname) {
+		txtUserId.sendKeys(uname);
+	}
+
+	public void setPassword(String pwd) {
+		txtPassword.sendKeys(pwd);
+	}
+
+	public void clickSubmit(String uname) {
+		btnLogin.click();
+	}
+
+}
